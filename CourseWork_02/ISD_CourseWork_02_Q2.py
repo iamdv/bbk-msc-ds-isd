e58c091f4e3a81cb79d13200075c80b71fd4b179
DV <me@iamdv.io>
2016-12-24 02:00:20 +0530

    Update ISD_CourseWork_02_Q2.py

--- CourseWork_02/ISD_CourseWork_02_Q2.py
+++ CourseWork_02/ISD_CourseWork_02_Q2.py
@@ -1,7 +1,8 @@
 #Please run the program directly. There is a print statement for each function.
+# Notes: Followed modular approach where one function is passed into other functions
 
 student=['adam','john','james','alice','sarah']
-module=['accounting','maths','geography','english']
+module=['cs','maths','geography','english']
 mark=[[90,-1,70,85],[-1,90,50,60],[40,70,-1,60],[75,80,60,70],[80,60,50,-1]]
 
 
@@ -44,6 +45,11 @@ def my_func_student_report_2_5(my_table):
     for each_student_name in student:
         student_average.append([each_student_name, my_func_avg_student_marks_2_3(my_table, each_student_name)])
     student_average = sorted(student_average, key=lambda x: x[1], reverse=True)
+    print('\n', '2.5: Printing Student Report')
+    print('\t', 'Student Name', '\t', 'Average Marks')
+    for my_row in student_average:
+        print('\t', my_row[0], '\t', '\t', '\t', my_row[1])
+    print('\n')
     return student_average
 
 
@@ -52,6 +58,11 @@ def my_func_module_report_2_6(my_table):
     for each_module_name in module:
         module_average.append([each_module_name, my_func_avg_module_marks_2_4(my_table, each_module_name)])
         module_average = sorted(module_average, key=lambda x: x[1], reverse=True)
+    print('\n', '2.6: Printing Module Report')
+    print('\t', 'Module Name', '\t', '\t','Average Marks')
+    for my_row in module_average:
+        print('\t', my_row[0], '\t', '\t', '\t', '\t', my_row[1])
+    print('\n')
     return module_average
 
 
@@ -66,7 +77,7 @@ print('2.3: Student Search: ', my_func_avg_student_marks_2_3(mark, 'test'))
 # Correct student names: ['adam','john','james','alice','sarah']
 
 print('2.4: Module Search: ', my_func_avg_module_marks_2_4(mark, 'accounting'))
-# Correct module names: ['accounting','maths','geography','english']
+# Correct module names: ['cs','maths','geography','english']
 
 print('2.5: Student Report: ', my_func_student_report_2_5(mark))
 # Displays student report as table (list of list) in descending order
